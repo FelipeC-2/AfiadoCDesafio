e1ef485ddf6f691349f0949872bf8717e8d36c64
FelipeC-2 <102688384+FelipeC-2@users.noreply.github.com>
2023-07-04 21:14:57 -0300

    Create CodigoFelipeCardozo.cs
    Anotando a lógica pra ter onde me lembrar.

---
+++ CodigoFelipeCardozo.cs
@@ -0,0 +1,18 @@
+using System;
+class Program {
+  static void Main() {
+    Console.WriteLine("Começo");
+    /* 
+    Pensamentos/raciocinio antes e durante implementação do código:
+    1. Para essa etapa, precisa pegar as strings na posição reversa, e colocá-las sem que a palavra fosse alterada(Ex: "amazing" -> "gnizama")
+    
+    2. Daria pra checar num loop for se i = j em que j percorre toda a string, remove o caractere quando ocorre essa igualdade
+    
+    3. Colocar os caracteres da string em um array, escrever uma string nova com essas linhas
+    
+    4. Usar o .ToUpper para tornar a letra maiúscula. Percorrer o código e se houver um espaço vazio e após este, uma letra, transformar em letra maiúscula. Para o começo da frase, usar a posição [0] pra transformar
+    
+    5. Similar ao 3, colocar os caracteres da string em um array, e verificar se uma string nova com os itens do array é igual à string original e se for, então true.
+    */
+  }
+}
